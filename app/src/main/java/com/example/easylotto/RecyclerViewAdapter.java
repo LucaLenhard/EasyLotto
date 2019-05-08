2903971564ac0994db939ca42c0bf3809e1626b8
Luca <luca.lenhard@web.de>
2019-05-08 22:51:18 +0200

    On click dialog workign now

--- app/src/main/java/com/example/easylotto/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/easylotto/RecyclerViewAdapter.java
@@ -2,6 +2,8 @@ package com.example.easylotto;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -31,12 +33,26 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         v = LayoutInflater.from(mContext).inflate(R.layout.item_gamefinal, viewGroup, false);
         final MyViewHolder vHolder = new MyViewHolder(v);
 
+        //Dialog init
+
+        myDialog = new Dialog(mContext);
+        myDialog.setContentView(R.layout.dialog_spiel);
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+
         vHolder.item_gamefinal.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-
-                Toast.makeText(mContext, "Test Click" + String.valueOf(vHolder.getAdapterPosition()), Toast.LENGTH_SHORT).show();
+                TextView dialog_volumen_tv = (TextView) myDialog.findViewById(R.id.dialog_volumen_id);
+                TextView dialog_buyin_tv = (TextView) myDialog.findViewById(R.id.dialog_buyin_id);
+                TextView dialog_datum_tv = (TextView) myDialog.findViewById(R.id.dialog_datum_id);
+                dialog_volumen_tv.setText(Integer.toString(mData.get(vHolder.getAdapterPosition()).getVolumen()));
+                dialog_buyin_tv.setText(Integer.toString(mData.get(vHolder.getAdapterPosition()).getSpieleranzahl()));
+                dialog_datum_tv.setText(mData.get(vHolder.getAdapterPosition()).getZiehungsdatum());
+                String Spielnummer_temp = mData.get(vHolder.getAdapterPosition()).getSpielnummer();
+                Toast.makeText(mContext, "Spiel " + Spielnummer_temp + " ausgewählt", Toast.LENGTH_SHORT).show();
+                myDialog.show();
             }
         });
 
